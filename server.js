3e900c88de9b730ebaccc5b828cb110e865c4830
EducatedSuddenBucket <educatedsuddenbucket@gmail.com>
2025-07-20 18:32:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const { createCanvas, loadImage, registerFont } = require('canvas');
 const path = require('path');
 const fs = require('fs');
 const axios = require('axios');
-const path = require('path');
 
 registerFont(path.join(__dirname, 'font.ttf'), { family: 'NotoSans' });
 const app = express();
